96eb3857cc4ae960ba0f2e13d540fbdd303ef7d6
Dekroo <dekroostream@gmail.com>
2024-12-23 17:25:27 +0100

    Buttons, images, text UPDATE
    Deleted old placeholder for image, added functionality for buttons, made new strings for titles and authors

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.artspace
 
-import android.graphics.Color
-import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -18,17 +15,18 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -57,6 +55,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtTimeLayout() {
+    var result by remember { mutableIntStateOf(1) } //remember to import getValue to make it work
+
+    val titleResource = when (result) {
+        1 -> R.string.art_title_1
+        2 -> R.string.art_title_2
+        3 -> R.string.art_title_3
+        4 -> R.string.art_title_4
+        else -> R.string.art_title_5
+    }
+
+    val authorResource = when (result) {
+        1 -> R.string.author_name_1
+        2 -> R.string.author_name_2
+        3 -> R.string.author_name_3
+        4 -> R.string.author_name_4
+        else -> R.string.author_name_5
+    }
+
+    val imageResource = when (result) {
+        1 -> R.drawable.art_image_1
+        2 -> R.drawable.art_image_2
+        3 -> R.drawable.art_image_3
+        4 -> R.drawable.art_image_4
+        else -> R.drawable.art_image_5
+
+    }
 
     Column (
         modifier = Modifier
@@ -66,7 +90,7 @@ fun ArtTimeLayout() {
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = stringResource(R.string.art_title),
+            text = "\"${stringResource(titleResource)}\"", //quotation marks
             style = TextStyle(
                 fontWeight = FontWeight.Bold,//bold font
                 fontSize = 30.sp
@@ -81,6 +105,7 @@ fun ArtTimeLayout() {
             modifier = Modifier
                 .wrapContentSize()
         ) {
+
 //I HAVE NO IDEA HOW TO DO BLURRED BORDER AROUND IMAGE AS OF NOW
 //            val borderBlur = @Composable {
 //                Box(modifier = Modifier
@@ -92,28 +117,34 @@ fun ArtTimeLayout() {
 //            }
 //            borderBlur()
 
+//UNUSED BORDER, IF YOU WANT TO USE IT REPLACE BorderStroke WITH IT
+//    val borderBrush = Brush.linearGradient(
+//        0.0f to Color.Black,
+//        1.0f to Color.Gray,
+//        start = Offset(100.0f, 0.0f),
+//        end = Offset(0.0f, 100.0f)
+//    )
+
             Image(
-                painter = painterResource(R.drawable._448_profilecover_1682290871382),
-                contentDescription = "asdsad",
+                painter = painterResource(imageResource),
+                contentDescription = "",
                 modifier = Modifier
                     .padding(5.dp)
                     .border(
                         border = BorderStroke(
                             width = 2.dp,
-                            color = colorResource(id = R.color.black)
+                            color = Color.Black
                         )
                     )
                     .wrapContentSize()
-//                    .blur(5.dp)
             )
         }
 
-
         Text(
-            text = stringResource(R.string.author_name),
+            text = stringResource(authorResource),
             style = TextStyle(
                 fontWeight = FontWeight.Light,
-                fontSize = 17.sp
+                fontSize = 20.sp
             ),
             modifier = Modifier
                 .align(alignment = Alignment.CenterHorizontally)
@@ -128,7 +159,9 @@ fun ArtTimeLayout() {
                 .fillMaxHeight()
         ) {
             Button(
-                onClick = { /*TODO: Wprowadź logikę kliknięcia tutaj */ },
+                onClick = {
+                        result = if (result > 1) result - 1 else 1
+                },
                 modifier = Modifier
                     .padding(top = 16.dp)
                     .weight(1f) //by using with .weight (on buttons modifiers) the buttons are the same size
@@ -136,7 +169,9 @@ fun ArtTimeLayout() {
                 Text(text = "Previous")
             }
             Button (
-                onClick = { /*TODO*/},
+                onClick = {
+                    result = if (result < 5) result + 1 else 1 //when the result is smaller than 5 increment, else return to 1
+                },
                 modifier = Modifier
                     .padding(top=16.dp)
                     .weight(1f)
